468d6b74395a708cfe6bcfdc1eca3996889b129d
Mikal Shrestha <mikal.shrestha@f1soft.com>
2021-01-01 09:05:20 +0545

    smallest kth element in array

--- Problem-3/Solution.java
+++ Problem-3/Solution.java
@@ -1,62 +1,22 @@
-// 
-class Solution { 
-    
-	// Utility function to print the contents of an array 
-	static void printArr(int arr[], int n) 
+import java.util.Arrays; 
+import java.util.Collections;
+
+public class Solution {
+
+	public static int kthSmallest(Integer[] arr, int k) 
 	{ 
-		for (int i = 0; i < n; i++) 
-			System.out.print(arr[i] + " "); 
+		// Sort the given array 
+		Arrays.sort(arr); 
+
+		// Return k'th element in 
+		// the sorted array 
+		return arr[k - 1]; 
 	} 
-	
-	// Function to sort the array of 0s, 1s and 2s 
-	static void sortArr(int arr[], int n) 
-	{ 
-		int i, cnt0 = 0, cnt1 = 0, cnt2 = 0; 
-	
-		// Count the number of 0s, 1s and 2s in the array 
-		for (i = 0; i < n; i++) { 
-			switch (arr[i]) { 
-			case 0: 
-				cnt0++; 
-				break; 
-			case 1: 
-				cnt1++; 
-				break; 
-			case 2: 
-				cnt2++; 
-				break; 
-			} 
-		} 
-	
-		// Update the array 
-		i = 0; 
-	
-		// Store all the 0s in the beginning 
-		while (cnt0 > 0) { 
-			arr[i++] = 0; 
-			cnt0--; 
-		} 
-	
-		// Then all the 1s 
-		while (cnt1 > 0) { 
-			arr[i++] = 1; 
-			cnt1--; 
-		} 
-	
-		// Finally all the 2s 
-		while (cnt2 > 0) { 
-			arr[i++] = 2; 
-			cnt2--; 
-		} 
-	
-		// Print the sorted array 
-		printArr(arr, n); 
-	} 
-	
+
 	public static void main(String[] args) 
 	{ 
-		int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 }; 
-		int n = arr.length; 
-		sortArr(arr, n); 
+		Integer arr[] = new Integer[] { 12, 3, 5, 7, 19 }; 
+		int k = 2; 
+		System.out.print("K'th smallest element is " + kthSmallest(arr, k)); 
 	} 
 } 
